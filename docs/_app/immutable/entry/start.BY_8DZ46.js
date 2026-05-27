f7bc215dd1b9a8f2a6e53481ba8941dfac861952
Cayle Bray <cayle.bray@braymatter.com>
2026-05-27 12:55:08 -0500

    Add docs-based Pages build

---
+++ docs/_app/immutable/entry/start.BY_8DZ46.js
@@ -0,0 +1,1 @@
+import{l as o,s as r}from"../chunks/B0z1gxxR.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const output = process.env.PAGES_OUTPUT ?? 'build';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
+      pages: output,
+      assets: output,
       fallback: '404.html'
     }),
     paths: {
